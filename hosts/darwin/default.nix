9108fc0f4d7816f429d5d1e1baab7b52042124c3
Ian Montgomery <ian@ianmontgomery.dev>
2024-09-05 23:16:19 +1200

    Merging more of my config changes

--- hosts/darwin/default.nix
+++ hosts/darwin/default.nix
@@ -1,14 +1,13 @@
-{ agenix, config, pkgs, ... }:
+{ inputs, config, pkgs, ... }:
 
-let user = "dustin"; in
+let user = "ian"; in
+
 {
 
   imports = [
-    ../../modules/darwin/secrets.nix
     ../../modules/darwin/home-manager.nix
     ../../modules/shared
     ../../modules/shared/cachix
-     agenix.darwinModules.default
   ];
 
   # Auto upgrade nix package and the daemon service.
@@ -37,30 +36,16 @@ let user = "dustin"; in
 
   # Load configuration that is shared across systems
   environment.systemPackages = with pkgs; [
-    emacs-unstable
-    agenix.packages."${pkgs.system}".default
+
   ] ++ (import ../../modules/shared/packages.nix { inherit pkgs; });
-
-  launchd.user.agents.emacs.path = [ config.environment.systemPath ];
-  launchd.user.agents.emacs.serviceConfig = {
-    KeepAlive = true;
-    ProgramArguments = [
-      "/bin/sh"
-      "-c"
-      "{ osascript -e 'display notification \"Attempting to start Emacs...\" with title \"Emacs Launch\"'; /bin/wait4path ${pkgs.emacs}/bin/emacs && { ${pkgs.emacs}/bin/emacs --fg-daemon; if [ $? -eq 0 ]; then osascript -e 'display notification \"Emacs has started.\" with title \"Emacs Launch\"'; else osascript -e 'display notification \"Failed to start Emacs.\" with title \"Emacs Launch\"' >&2; fi; } } &> /tmp/emacs_launch.log"
-    ];
-    StandardErrorPath = "/tmp/emacs.err.log";
-    StandardOutPath = "/tmp/emacs.out.log";
-  };
-
+  
+  # Add ability to used TouchID for sudo authentication
+  security.pam.enableSudoTouchIdAuth = true;
+  
   system = {
     stateVersion = 4;
 
     defaults = {
-      LaunchServices = {
-        LSQuarantine = false;
-      };
-
       NSGlobalDomain = {
         AppleShowAllExtensions = true;
         ApplePressAndHoldEnabled = false;
@@ -80,7 +65,6 @@ let user = "dustin"; in
         autohide = false;
         show-recents = false;
         launchanim = true;
-        mouse-over-hilite-stack = true;
         orientation = "bottom";
         tilesize = 48;
       };
